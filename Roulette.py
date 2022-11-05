6d3f447951fb58a7d59ecaf6a230d435fea65a99
DevEvilUser <117503500+DevEvilUser@users.noreply.github.com>
2022-11-05 10:49:40 +0300

    Update Roulette.py

--- Roulette.py
+++ Roulette.py
@@ -1,1 +1,36 @@
+
+# .------.------.------.------.------.------.------.------.------.------.
+# |D.--. |4.--. |N.--. |1.--. |3.--. |L.--. |3.--. |K.--. |0.--. |0.--. |
+# | :/\: | :/\: | :(): | :/\: | :(): | :/\: | :(): | :/\: | :/\: | :/\: |
+# | (__) | :\/: | ()() | (__) | ()() | (__) | ()() | :\/: | :\/: | :\/: |
+# | '--'D| '--'4| '--'N| '--'1| '--'3| '--'L| '--'3| '--'K| '--'0| '--'0|
+# `------`------`------`------`------`------`------`------`------`------'
 #
+#                     Copyright 2022 t.me/D4n13l3k00
+#           Licensed under the Creative Commons CC BY-NC-ND 4.0
+#
+#                    Full license text can be found at:
+#       https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode
+#
+#                           Human-friendly one:
+#            https://creativecommons.org/licenses/by-nc-nd/4.0
+
+# meta developer: @evilginx
+
+
+import random
+import re
+
+from .. import loader, utils
+
+
+@loader.tds
+class RouletteMod(loader.Module):
+    strings = {"name": "Генератор списка участников"}
+    prefix = "<b>[GLU]</b>\n"
+
+    @loader.owner
+    async def glucmd(self, m):
+        ".glu <int> - максимальное количество участников"
+        args = utils.get_args_raw(m)
+        
